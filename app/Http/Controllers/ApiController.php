044872cc6f7ae0820196572fac3e1bce44d8aa4d
g9308370 <g9308370@hotmail.com>
2015-06-08 14:05:20 +0800

    Trace Other's API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,25 +2,29 @@
 
 use Laravel\Lumen\Routing\Controller as BaseController;
 use App\Apis\Everybody\SMSCurl;
+use Illuminate\Http\Request;
 
 class ApiController extends BaseController
 {
+
     //
     public function index(){
 
     	return 'You got the ApiController';
 
     }
-    public function every8d(){
+    public function every8d(Request $request){
 
     	$everybody = new SMSCurl('hchs','jkl45682');
     	$everybody->setTarget([
     		'0916023011',
-    		'0931055090'
+    		'0931055090',
+    		'0982704434'
     		]);
     	$everybody->setMessage('我在測試中文簡訊');
-    	$everybody->send();
-    	dd($everybody->getResponse());
+    	//$everybody->send();
+    	dd($request);
     	return view('every8d.index');
     }
+
 }
